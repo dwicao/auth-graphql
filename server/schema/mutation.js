5717ba1313ccbec898d7f3a8ce4c1630ccccd165
dwicao <lutfiandwica@gmail.com>
2017-06-06 21:34:06 +0700

    delegating to auth services

---
+++ server/schema/mutation.js
@@ -0,0 +1,25 @@
+const graphql = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+} = graphql;
+const UserType = require('./types/user_type');
+const AuthServices = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        signup: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString },
+            },
+            resolve(parentValue, { email, password }, req) {
+                return AuthServices.signup({ email, password, req });
+            },
+        },
+    },
+});
+
+module.exports = mutation;
